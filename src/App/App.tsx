4735169caa4977ab2f42dea7f651b58c5b778ef4
tuosha <tuosha@yandex.ru>
2024-11-29 13:50:42 +0300

    add Loadable wrapper to Suspense provider add alternative lazy loading (from Component)

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -9,6 +9,7 @@ const App = () => {
             <Link to={'/state-as-object'}>State as object</Link>
             <Link to={'/change-div-color-by-input'}>Change div color by input</Link>
             <Link to={'/children-as-props'}>Children as props</Link>
+            <Link to={'/lazy-loading'}>Lazy loading</Link>
             <Outlet />
         </div>
     )
---
+++ src/LazyComponentLoading/LazyText.tsx
@@ -0,0 +1,9 @@
+const LazyText = () => {
+    return (
+        <div>
+            <h2>I`m lazy component</h2>
+        </div>
+    )
+}
+
+export default LazyText
