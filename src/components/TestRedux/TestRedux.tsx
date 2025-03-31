6b031eefdffc8af1293ec488d64c6ccd9f23bfd2
tuosha <tuosha@yandex.ru>
2025-03-31 18:50:09 +0300

    little fixes

--- src/components/TestRedux/TestRedux.tsx
+++ src/components/TestRedux/TestRedux.tsx
@@ -6,10 +6,10 @@ const TestRedux = () => {
     const dispatch = useDispatch()
     const cash = useSelector(getCashValue)
     const onAddCash = () => {
-        dispatch(cashActions.addCash(10))
+        dispatch(cashActions.addCash(Number(prompt('введите значение'))))
     }
     const onGetCash = () => {
-        dispatch(cashActions.getCash(20))
+        dispatch(cashActions.getCash(Number(prompt('введите значение'))))
     }
     return (
         <div>
